e1bd7fd58796a5f9a5b79926fd096b1e0cad7cbd
jeremysun1224 <2113751293@qq.com>
2020-12-24 16:20:48 +0800

    Update the running process on Colab

--- run_dureader_roberta_large.sh
+++
@@ -1,8 +0,0 @@
-export PYTHONIOENCODING=utf-8
-export DATA_DIR=/env/comprehension_contest/dureader_robust-data
-export TASK_NAME=DuReader
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-export OUTPUT_DIR=./roberta_large_adv_output2
-# export MODEL_NAME_OR_PATH=$OUTPUT_DIR/$TASK_NAME/checkpoint-5000/
-export MODEL_NAME_OR_PATH=chinese_wwm_ext_pytorch_large
-python main.py --model_type bert --model_name_or_path $MODEL_NAME_OR_PATH --do_train --data_dir=$DATA_DIR --max_seq_length 256 --per_gpu_train_batch_size 4 --do_lower_case --learning_rate 2e-5 --num_train_epochs 10.0 --output_dir $OUTPUT_DIR/$TASK_NAME/ --save_steps 1000 --overwrite_output_dir --max_answer_length 30
